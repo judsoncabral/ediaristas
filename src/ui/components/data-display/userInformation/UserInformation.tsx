21bb6df9672ac1d75a736e88c0c771d4a4bcea48
Judson Cabral de Melo <judson.cabral.010@ufrn.edu.br>
2021-10-11 18:45:09 -0300

    Adicionando parte dos usuarios

---
+++ src/ui/components/data-display/userInformation/UserInformation.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  UserInformationContainer,
+  AvatarStyled,
+  RatingStyled,
+  UserName,
+  UserDescription,
+} from "./UserInformation.style";
+
+interface UserInformationProps {
+  picture?: string;
+  name: string;
+  rating: number;
+  description?: string;
+}
+
+const UserInformation: React.FC<UserInformationProps> = ({
+  name,
+  picture,
+  rating,
+  description,
+}) => {
+  return (
+    <UserInformationContainer>
+      <AvatarStyled src={picture}>{name[0]}</AvatarStyled>
+      <UserName> {name} </UserName>
+      <RatingStyled readOnly value={rating} />
+      <UserDescription> {description} </UserDescription>
+    </UserInformationContainer>
+  );
+};
+
+export default UserInformation;
